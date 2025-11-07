e95a9c116424e03ec846396137d1a9ae0724a05a
mukulsingh24 <rmks1004@gmail.com>
2025-11-07 21:21:05 +0530

    backend server created

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 5000
+app.use(cors())
+
+app.listen(port,()=>{
+    console.log(`Server is running at ${port}`);
+})
